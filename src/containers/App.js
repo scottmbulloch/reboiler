d2c0eb6a79c2641de96be44b9521d49bead504e2
Scott Williams <scott@swilliams.me>
2016-11-28 15:25:27 -0700

    moves files

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './stylesheets/App.scss';
+import '../stylesheets/App.scss';
 
 class App extends Component {
   render() {
--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { expect } from 'chai'
-import App from '../src/App'
+import App from './App'
 
 const wrapper = shallow(<App />);
 
