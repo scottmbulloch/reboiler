68c53d606d1e69954e4456ea855b45de3d311533
Scott Williams <scott@swilliams.me>
2016-11-28 15:09:00 -0700

    mv test/ to src/

--- scripts/test.js
+++ scripts/test.js
@@ -10,6 +10,20 @@ require('dotenv').config({silent: true});
 const argv = process.argv.slice(2);
 
 // Watch unless on CI
-if (!process.env.CI) {
-  argv.push('--watch');
+// if (!process.env.CI) {
+//   argv.push('--watch');
+// }
+import jsdom from 'jsdom';
+
+global.document = jsdom.jsdom('<html><body></body></html>');
+global.window = document.defaultView;
+global.navigator = window.navigator;
+
+function noop() {
+  return {};
 }
+
+// prevent mocha tests from breaking when trying to require a css file
+require.extensions['.css'] = noop;
+require.extensions['.svg'] = noop;
+require.extensions['.scss'] = noop;
