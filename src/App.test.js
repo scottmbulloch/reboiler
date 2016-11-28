f69a502c00308185094b4ac55ce12a4de3c143c4
Scott Williams <scott@swilliams.me>
2016-11-28 15:08:00 -0700

    mv test/ to src/

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { shallow } from 'enzyme'
+import { expect } from 'chai'
+import App from '../src/App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+const wrapper = shallow(<App />);
+
+describe('(Component) App', () => {
+  it('renders...', () => {
+    expect(wrapper).to.have.length(1);
+  });
 });
