a8642e40f8e3be6523f7c0f14aaa6c0aa9bb1176
Scott Williams <scott@swilliams.me>
2016-12-02 09:43:09 -0700

    Adds comments

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import '../stylesheets/App.scss';
 
+// This is more of a 'layout' kind of component.
 class App extends Component {
   render() {
     return (
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,9 @@ import Home from './components/home';
 import About from './components/about';
 import NotFoundPage from './components/NotFoundPage';
 
+// 'App' is the Layout 'view'.
+// The default component it shows is Home (the IndexRoute).
+// About is a basic page, and everything else goes to NotFoundPage.
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Home} />
